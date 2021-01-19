a59f94c3dd1e2d816dd17b3bb10f34f54f11cb76
MSoup <19704779+MSoup@users.noreply.github.com>
2021-01-19 21:27:13 +0900

    refactored into factory function

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
-// Main app
+// Book constructor
+function Book(title, author, pages, read) {
+  return { title, author, pages, read };
+}
 
+// query selectors
 const form = document.querySelector(".form");
-let library = [];
-
-const submitButton = document.querySelector(".submit");
 const messageBox = document.querySelector(".message-box");
-const displayBooks = document.querySelector(".window-container");
+const libraryDisplayContainer = document.querySelector(
+  ".library-display-container"
+);
+const card = document.querySelector(".card");
+const statusBox = document.querySelector(".status");
 
-submitButton.addEventListener("click", submitHandler);
+// event listeners
+form.addEventListener("click", submitHandler);
+document.addEventListener("click", handleChangeStatus);
 
-// creates object with submitted values and sends to global book list
-function submitHandler() {
+function submitHandler(event) {
+  if (!event.target.className.includes("button")) {
+    return;
+  }
+
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
   let read = document.getElementById("read").checked;
+
+  console.log(read);
 
   // validate
   if (pages && !Number.isInteger(parseInt(pages))) {
@@ -24,16 +36,12 @@ function submitHandler() {
     displayMessage("must include title", true);
     return;
   }
-  let book = {
-    title: title,
-    author: author,
-    pages: parseInt(pages),
-    read: read,
-  };
-  // debugging
+
+  // bundle form -> append to list
+  let book = new Book(title, author, pages, read);
+  Library.addBook(book);
+
   displayMessage("Added: " + book.title);
-  library.push(book);
-  displayLibrary();
   clearForm();
 }
 
@@ -46,19 +54,41 @@ function displayMessage(msg, err = false) {
   messageBox.textContent = msg;
 }
 
-function displayLibrary() {
-  library.forEach((book) => {
-    console.table(book);
-    displayBooks.append(book);
-  });
+function clearForm() {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pages").value = "";
 }
 
-function clearForm() {
-  let title = document.getElementById("title");
-  let author = document.getElementById("author");
-  let pages = document.getElementById("pages");
+// factory function Library -> lets me call addBook and removeBook as of now
+const Library = (function () {
+  let myModule = {};
+  let books = [];
+  let _displayBooks = function () {
+    console.log(books);
+    libraryDisplayContainer;
+  };
+  myModule.addBook = function (title, author = "", pages = "", read = false) {
+    let book = books.push({ title, author, pages, read });
+    _displayBooks();
+  };
 
-  title.value = "";
-  author.value = "";
-  pages.value = "";
+  myModule.removeBook = function (id) {
+    books.splice(id, 1);
+    _displayBooks();
+  };
+
+  myModule.displayBooks = function () {
+    _displayBooks();
+  };
+  return myModule; // returns the Object with public methods
+})();
+
+// usage
+Library.displayBooks();
+
+function handleChangeStatus(event) {
+  if (event.target.className.includes("status")) {
+    event.target.querySelector(".dot").classList.toggle("read");
+  }
 }
