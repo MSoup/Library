019fe27b84d576424b3f66a63ab27ea5ededba8c
MSoup <19704779+MSoup@users.noreply.github.com>
2021-01-23 12:20:58 +0900

    responsive added

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const statusBox = document.querySelector(".status");
 
 // event listeners
 form.addEventListener("click", submitHandler);
-document.addEventListener("click", handleChangeStatus);
+libraryDisplayContainer.addEventListener("click", handleChangeStatus);
 
 function submitHandler(event) {
   if (!event.target.className.includes("button")) {
@@ -24,19 +24,22 @@ function submitHandler(event) {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
-  let read = document.getElementById("read").checked ? true : false;
-  let id = Library.getLength + 1;
+  let read = document.getElementById("read").checked;
+  let id = Library.getLength();
 
   // validate
-  if (pages && !Number.isInteger(parseInt(pages))) {
-    displayMessage("Error: pages has to be a number", true);
-    return;
-  } else if (!title) {
+  if (!author) {
+    author = "author unknown";
+  }
+  if (!pages || parseInt(pages) == "NaN") {
+    pages = "? pages";
+  }
+  if (!title) {
     displayMessage("Error: must include title", true);
     return;
   }
 
-  // bundle form -> append to list
+  // bundle form -> append to nodelist
   let book = new Book(title, author, pages, read, id);
   Library.addBook(book);
 
@@ -62,13 +65,14 @@ function clearForm() {
 // factory function Library -> lets me call addBook and removeBook as of now
 const Library = (function () {
   let myModule = {};
-  // make books a nodeList????
+  // books is a nodeList
   let books = libraryDisplayContainer;
 
   // methods
   myModule.addBook = function (bookObj) {
     // make DOM object from bookObj
     let newCard = card.cloneNode(true);
+    newCard.classList.remove("hidden");
     // modify title, author, pages, read, id
     newCard.querySelector(".title").textContent = bookObj.title;
     newCard.querySelector(".author").textContent = bookObj.author;
@@ -76,14 +80,17 @@ const Library = (function () {
     if (bookObj.read) {
       newCard.querySelector(".dot").classList.add("read");
     }
+    newCard.querySelector(".id").textContent = bookObj.id;
     books.append(newCard);
   };
 
   myModule.removeBook = function (id) {
-    books.splice(id, 1);
+    console.log(books.querySelector(".id"));
   };
 
-  myModule.getLength = () => books.length;
+  myModule.getLength = function () {
+    return books.querySelectorAll(".id").length;
+  };
 
   myModule.displayBooks = function () {
     return console.log(books);
@@ -91,8 +98,36 @@ const Library = (function () {
   return myModule; // returns the Object with public methods
 })();
 
-function handleChangeStatus(event) {
-  if (event.target.className.includes("status")) {
-    event.target.querySelector(".dot").classList.toggle("read");
+function handleChangeStatus(e) {
+  if (e.target.closest(".status")) {
+    e.target.closest(".status").querySelector(".dot").classList.toggle("read");
+    return;
+  }
+  if (e.target.closest("div > .test2")) {
+    console.log("deleted");
+    return;
+  }
+
+  if (e.target.closest("div > .test1")) {
+    console.log("edit");
+    // toss info into popup form and allow edits?
+    return;
+  }
+
+  if (e.target.closest("div > .test")) {
+    console.log("clone");
+    // toss info into popup form and allow edits?
+    return;
+  }
+
+  if (e.target.closest(".pic-container> img")) {
+    console.log("edit image");
+    return;
   }
 }
+
+// hide first card
+
+libraryDisplayContainer.querySelector(".card").classList.add("hidden");
+
+// intro message
